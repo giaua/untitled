b536c007f50eba26c01b52c77943402568890bf8
giaua <gavrilenko85@gmail.com>
2015-12-07 17:07:52 +0200

    dfgdg

---
+++ dowloadig_files.py
@@ -0,0 +1,12 @@
+import requests
+import os
+with open(os.path.join('/Users/admin/Documents/python/dataset_3378_3.txt'), 'r') as inf:
+    url = inf.readline().strip()
+first = requests.get(url).text[:2]
+while first != 'We':
+    file_name = requests.get(url).text
+    first = requests.get(url).text[:2]
+    url = 'https://stepic.org/media/attachments/course67/3.6.3/' + file_name
+    print(file_name)
+with open(os.path.join('/Users/admin/Documents/python/output2.txt'), 'w') as ouf:
+    ouf.write(requests.get(url).text)
