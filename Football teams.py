7b128113ca9325fece40dd765d10918aac17ed39
giaua <gavrilenko85@gmail.com>
2015-12-07 23:51:16 +0200

    sdfds

--- Football teams.py
+++ Football teams.py
@@ -1,5 +1,18 @@
 import os
-with open(os.path.join('/Users/admin/Documents/python/input.txt'), 'r') as inf:
+txt = []
+with open(os.path.join('C:\py3eg\input.txt'), 'r') as inf:
     for line in inf:
-        txt = [i.strip(';').split() for i in line]
+        txt.append([i.strip() for i in line.split(';')])
+"""x = int(input())
+txt = [[0 for j in range(4)] for i in range(x)]
+for i in range(x):
+    txt[i] = ([k for k in input().split()])"""
+dic = {}
 print(txt)
+for i in txt:
+    if i == '3':
+        continue
+    if i[0] in dic:
+        if i[1] > i[3]:
+            dic[i].append()
+
