7d0d2f2c54bcd00089fbc1c576ea45a3feef4e75
giaua <gavrilenko85@gmail.com>
2015-12-08 21:40:18 +0200

    sdfds

--- Football teams.py
+++ Football teams.py
@@ -1,18 +1,28 @@
-import os
-txt = []
-with open(os.path.join('C:\py3eg\input.txt'), 'r') as inf:
-    for line in inf:
-        txt.append([i.strip() for i in line.split(';')])
-"""x = int(input())
-txt = [[0 for j in range(4)] for i in range(x)]
-for i in range(x):
-    txt[i] = ([k for k in input().split()])"""
 dic = {}
-print(txt)
-for i in txt:
-    if i == '3':
-        continue
-    if i[0] in dic:
-        if i[1] > i[3]:
-            dic[i].append()
-
+x = int(input())
+for i in range(x):
+    team_1, score_1, team_2, score_2 = (i for i in input().split(';'))
+    if team_1 not in dic.keys():
+        dic[team_1] = [0 for i in range(5)]
+    if team_2 not in dic.keys():
+        dic[team_2] = [0 for i in range(5)]
+    if score_1 > score_2:
+        dic[team_1][1] += 1
+        dic[team_1][4] += 3
+        dic[team_2][3] += 1
+    elif score_1 < score_2:
+        dic[team_2][1] += 1
+        dic[team_2][4] += 3
+        dic[team_1][3] += 1
+    else:
+        dic[team_1][4] += 1
+        dic[team_2][4] += 1
+        dic[team_1][2] += 1
+        dic[team_2][2] += 1
+    dic[team_1][0] += 1
+    dic[team_2][0] += 1
+for key in dic.keys():
+    print(key, ':', sep='', end='')
+    for i in dic[key]:
+        print(i, end=' ')
+    print()
